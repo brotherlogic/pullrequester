b66152b1879c30b3b99c5e1fe7064283f0641ccd
Simon Tucker <simon@brotherlogic@gmail>
2019-07-23 18:48:33 -0700

    Fixes test approach

--- pullrequesterapi.go
+++ pullrequesterapi.go
@@ -4,6 +4,14 @@ import "golang.org/x/net/context"
 
 import pb "github.com/brotherlogic/pullrequester/proto"
 
+func (s *Server) updateChecks(check *pb.PullRequest_Check, req *pb.PullRequest) {
+	if check.Pass != pb.PullRequest_Check_PASS {
+		for _, checkFail := range req.Checks {
+			checkFail.Pass = pb.PullRequest_Check_FAIL
+		}
+	}
+}
+
 func (s *Server) update(ctx context.Context, req, reqIn *pb.PullRequest) (*pb.UpdateResponse, error) {
 	if reqIn.NumberOfCommits > 0 {
 		req.NumberOfCommits = reqIn.NumberOfCommits
@@ -15,17 +23,13 @@ func (s *Server) update(ctx context.Context, req, reqIn *pb.PullRequest) (*pb.Up
 			if checkIn.Source == check.Source {
 				found = true
 				checkIn.Pass = check.Pass
-
-				if check.Pass != pb.PullRequest_Check_PASS {
-					for _, checkFail := range req.Checks {
-						checkFail.Pass = pb.PullRequest_Check_FAIL
-					}
-				}
+				s.updateChecks(check, req)
 			}
 		}
 
 		if !found {
 			req.Checks = append(req.Checks, check)
+			s.updateChecks(check, req)
 		}
 	}
 
--- pullrequesterapi_test.go
+++ pullrequesterapi_test.go
@@ -39,7 +39,17 @@ func TestAddUpdate(t *testing.T) {
 		t.Errorf("New pull request was added not appended")
 	}
 
-	_, err = s.UpdatePullRequest(context.Background(), &pb.UpdateRequest{Update: &pb.PullRequest{Url: "blah", NumberOfCommits: 5, Checks: []*pb.PullRequest_Check{&pb.PullRequest_Check{Source: "blahs"}}}})
+	_, err = s.UpdatePullRequest(context.Background(), &pb.UpdateRequest{Update: &pb.PullRequest{Url: "blah", NumberOfCommits: 5, Checks: []*pb.PullRequest_Check{&pb.PullRequest_Check{Source: "blahs", Pass: pb.PullRequest_Check_PASS}}}})
+
+	if err != nil {
+		t.Errorf("New Pull request was not tracked!")
+	}
+
+	if len(s.config.Tracking) != 1 {
+		t.Errorf("New pull request was added not appended")
+	}
+
+	_, err = s.UpdatePullRequest(context.Background(), &pb.UpdateRequest{Update: &pb.PullRequest{Url: "blah", NumberOfCommits: 5, Checks: []*pb.PullRequest_Check{&pb.PullRequest_Check{Source: "blahtoo", Pass: pb.PullRequest_Check_FAIL}}}})
 
 	if err != nil {
 		t.Errorf("New Pull request was not tracked!")
@@ -49,8 +59,12 @@ func TestAddUpdate(t *testing.T) {
 		t.Errorf("Append happened this time")
 	}
 
-	if len(s.config.Tracking[0].Checks) != 1 {
+	if len(s.config.Tracking[0].Checks) != 2 {
 		t.Errorf("Too many checks!")
+	}
+
+	if s.config.Tracking[0].Checks[0].Pass == pb.PullRequest_Check_PASS {
+		t.Errorf("Prior pass should be nulled out")
 	}
 
 }
